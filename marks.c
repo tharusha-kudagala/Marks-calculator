ccaf0570f58e9f86df23bba9b0e38e7b8b8e7c63
tharusha-kudagala <44969527+tharusha-kudagala@users.noreply.github.com>
2018-12-01 11:23:09 +0530

    Add files via upload

---
+++ marks.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+int sub1,sub2,sub3;
+float avg;
+char name[10];
+
+printf("Enter your name :");
+scanf("%s",&name);
+
+printf("Enter your marks for subjects");
+scanf("%d %d %d",&sub1,&sub2,&sub3);
+
+avg=(sub1+sub2+sub3)/3.00;
+
+if (avg<=100 && avg>=75)
+	printf("Your grade is 'A'\n");
+else if (avg>=60 && avg<75)
+	printf("Your grade is 'B'\n");
+else if (avg>=50 && avg<60)
+	printf("Your grade is 'C'\n");
+else if (avg>=35 && avg<50)
+	printf("Your grade is 'S'\n");
+else
+	printf("Your grade is 'F'\n");
+
+
+return 0;
+}
+
